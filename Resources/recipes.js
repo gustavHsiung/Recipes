514dc156928a517bb5e4fa059cdf6e6a1437446f
gustavHsiung <knightgustav@gmail.com>
2012-02-21 22:56:05 +0800

    Enhance TableView with custom row. - add image - description is not correct, because the description format is much more complicated than text. Will Try to fix it by add a webview to each row.

--- Resources/recipes.js
+++ Resources/recipes.js
@@ -34,12 +34,53 @@ recipesHTTPClient.onload = function() {
 	{
 		var content = items.item(i);
 		var title = content.getElementsByTagName("title").item(0).text;
+		var description = content.getElementsByTagName("description").item(0).text;
+		
 		Ti.API.info(title);
 	
 		//create table row
 		var row = Titanium.UI.createTableViewRow({
-			title: title
+			hasChild: true,
+			className: 'recipe-row',
+			height:'auto'
 		});
+		//title label for row at index i
+		var titleLabel = Titanium.UI.createLabel({
+			text: title,
+			font : {fontSize: 14, fontWeight : ' bold' },
+			left: 70,
+			top: 5,
+			height: 20,
+			width: 210
+		});
+		
+		row.add(titleLabel);
+		
+		//description label for row at index i
+		var descriptionLabel = Titanium.UI.createLabel({
+			font : {fontSize: 10, fontWeight : ' normal ' },
+			left: 70,
+			top: 25,
+			height: 40,
+			width: 200
+		});
+		
+		if(descriptionLabel.text == '') {
+			descriptionLabel.text = 'No description is available.'; 
+		}else{
+			descriptionLabel.text = description;			
+		}
+		row.add(descriptionLabel);
+		//add our little icon to the left of the row 
+		var iconImage = Titanium.UI.createImageView({
+			image: 'img/eggpan.png',
+			width: 50,
+			height: 50,
+			left: 10,
+			top: 10 
+		});
+		row.add(iconImage);
+		
 		//add the row to data array
 		data.push(row);
 	}
