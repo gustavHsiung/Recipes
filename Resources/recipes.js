98cc4c470da2bd41d20a3c29f4fe2e89a5cf4cb2
gustavHsiung <knightgustav@gmail.com>
2012-02-21 13:28:13 +0800

    Try to parse the data from the website, http://www.cuisine.com.au/feed/all-recipes, which is used by the example. However, the site has changed their content, it is not a simple http anymore, so the task couldn be completed. Next step is to try other sources.

--- Resources/recipes.js
+++ Resources/recipes.js
@@ -4,22 +4,53 @@
 
 var win = Titanium.UI.currentWindow; 
 
+//the data storage empty array
+var data = []; 
+
 //declare the ht tp cl ient object
-var xhr = Titanium.Network.createHTTPClient();
+var recipesHTTPClient = Titanium.Network.createHTTPClient();
+
+//create a table view
+var recipesTable = Titanium.UI.createTableView({
+	height: 366,
+	width: 	320,
+	top: 	0,
+	left: 	0
+}); 
+win.add(recipesTable);
 
 // this method will process the remote data 
-xhr.onload = function() {
+recipesHTTPClient.onload = function() {
 	Ti.API.info(this.responseText);
+	var xml = this.responseXML;
+	//get item node list
+	var items = xml.documentElement.getElementsByTagName("item");
+	
+	//get through each item 
+	for(var i = 0; i < items.length; i++)
+	{
+		//create table row
+		var row = Titanium.UI.createTableViewRow({
+			title: items.item(i).getElementsByTagName("Title").item(0).text
+		});
+		//add the row to data array
+		data.push(row);
+	}
+	// set the data to tableview's data
+	recipesTable.data = data;
 };
 
 //this method will fire if there's an error in accessing the //remote data
-xhr.onerror = function() {
+recipesHTTPClient.onerror = function() {
 	// log the error to our Ti tanium Studio console
 	Ti.API.error(this.status + ' - ' + this.statusText);
 }
 ;
 //open the recipes xml feed
-xhr.open('GET' , 'http://www.cuisine.com.au/feed/all-recipes');
+recipesHTTPClient.open('GET' , 'http://www.cuisine.com.au/feed/all-recipes');
 
 //execute the call to the remote feed 
-xhr.send();
+recipesHTTPClient.send();
+
+
+
